3d9f2be8bfb97c28166b09855f0a7e3961dd51c8
John Anthony Rivera <askinganthony@gmail.com>
2016-02-12 13:35:19 -0800

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,5 @@
 'use strict';
+function favIceCream(){return "I love mint chocolate chip"}
+function shouting(str){return str.toUpperCase()}
+function roundDown(num){return Math.floor(num)}
+function theTruth(){return true}
